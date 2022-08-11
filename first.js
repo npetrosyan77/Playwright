f5e04f0815a09d7fb982fbe0a90058b7499510e0
Narine Petrosyan <narinepetrosyan869@gmail.com>
2022-08-11 12:39:51 +0400

    First test

---
+++ first.js
@@ -0,0 +1,16 @@
+const {webkit, chromium, firefox} = require('playwright');
+
+(async() =>{
+    for(const browserType of [webkit, firefox, chromium]) {
+        const browser = await browserType.launch({
+            headless: false,
+        });
+        const page = await browser.newPage();
+        await page.goto('https://github.com/microsoft/playwright');
+        await page.screenshot({
+            path: `screenshot-${browserType.name()}.png`,
+        });
+        await browser.close();
+        console.log('success: ' + browserType.name());
+    }
+})();
